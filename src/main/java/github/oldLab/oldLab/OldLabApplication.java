492b6ebec80dcaaa2acf6deea3918a1f66fe548d
axgiri <reiaogiri@gmail.com>
2025-07-06 13:28:19 +0200

    initial commit

---
+++ src/main/java/github/oldLab/oldLab/OldLabApplication.java
@@ -0,0 +1,13 @@
+package github.oldLab.oldLab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OldLabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OldLabApplication.class, args);
+	}
+
+}
